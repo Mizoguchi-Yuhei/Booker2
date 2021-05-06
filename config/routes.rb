6d12c1d9e2b2fd5add7928b29471936c7e1e3123
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-05-06 12:37:38 +0000

    add controllers views db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root "home#top"
+  get '/homes/about' => "homes#about"
+
+  devise_for :users
+
+  resources :books
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
